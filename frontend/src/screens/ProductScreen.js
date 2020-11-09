f9e8fabae8df3d04bf58c512b8b1b7a6bcb1e582
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-11-09 12:57:47 +0000

    Update ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,21 +2,10 @@ import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from "axios"
-// import products from "../products";
+
 
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState({});
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data} = await axios.get(`/api/products/${match.params.id}`)
-  
-      setProduct(data)
-    }
-  
-    fetchProduct()
-  }, [match])
+ 
 
   return (
     <>
