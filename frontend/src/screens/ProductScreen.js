979ae992a879c0c8bdb323e34409dc5f5b5edab3
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-11-09 12:59:31 +0000

    Update ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,6 +6,7 @@ import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
+import { clearProductDetails, listProductDetails } from "../store/actions/productActions"
 
 
 const ProductScreen = ({ match }) => {
