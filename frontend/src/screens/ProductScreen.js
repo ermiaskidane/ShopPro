10536563fa1174b9c98988f1b00acdf45943d499
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-11-09 12:58:55 +0000

    Update ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,11 @@
 import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import {useDispatch, useSelector } from "react-redux";
+
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
 
 const ProductScreen = ({ match }) => {
