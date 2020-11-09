6b0d99e42ae6ed36beb1f6a34dc0ac51cadd796a
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-11-09 12:57:00 +0000

    Update HomeScreens.js

--- frontend/src/screens/HomeScreens.js
+++ frontend/src/screens/HomeScreens.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 
+
 import Message from "../components/Message"
 import Loader from "../components/Loader"
 
@@ -22,16 +23,23 @@ useEffect(() => {
 
   return (
     <>
-      <Row>
+    <h1>Latest Products</h1>
+    {loading ?
+     <Loader/> 
+     : error ? 
+     <Message variant="danger">{error}</Message>:
+     <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
-      </Row>
+      </Row>} 
     </>
   );
 };
 
 export default HomeScreens;
+
+
 
