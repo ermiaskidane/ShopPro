592c1c830023b09cc394915baed1b0109172ee95
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-11-09 12:18:46 +0000

    Update products.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -12,7 +12,6 @@ const products = [
       numReviews: 12,
     },
     {
-      _id: "2",
       name: "iPhone 11 Pro 256GB Memory",
       image: "/images/phone.jpg",
       description:
@@ -25,7 +24,6 @@ const products = [
       numReviews: 8,
     },
     {
-      _id: "3",
       name: "Cannon EOS 80D DSLR Camera",
       image: "/images/camera.jpg",
       description:
