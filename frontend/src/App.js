7693c5077882b32f4b14d0a8c5789fc25e613293
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-11-10 13:19:37 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreens from "./screens/HomeScreens";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen"
 
 const App = () => {
   return (
