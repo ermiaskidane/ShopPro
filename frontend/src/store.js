5f58a292588885b5098f14951511f85e17628572
ermias kidane <hadeklte21@gmail.com>
2020-11-13 14:18:53 +0000

    checkout part one

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,15 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { productListReducer, productDetailsReducer } from "./store/reducers/productReducers"
-import { cartReducer } from "./store/reducers/cartReducers"
+import { cartReducer } from "./store/reducers/cartReducers"  
 import { 
     userLoginReducer, 
     userRegisterReducer, 
     userDetailsReducer,
     userUpdateProfileReducer
 } from "./store/reducers/userReducers"
-
+import { orderCreateReducer } from "./store/reducers/orderReducers"
+ 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
@@ -17,7 +18,8 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem("cartItems") ? 
@@ -25,10 +27,16 @@ const cartItemsFromStorage = localStorage.getItem("cartItems") ?
 
 const userInfoFromStorage = localStorage.getItem("userInfo") ? 
     JSON.parse(localStorage.getItem("userInfo")) : null
+
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress") ? 
+    JSON.parse(localStorage.getItem("shippingAddress")) : {}
  
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage},
-    userLogin: { userInfo: userInfoFromStorage }
+    cart: { 
+        cartItems: cartItemsFromStorage, 
+        shippingAddress: shippingAddressFromStorage 
+    },
+    userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
