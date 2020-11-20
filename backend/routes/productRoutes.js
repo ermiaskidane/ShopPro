634c1d279c1f8cb5b204044d319e73508d29ae6a
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-11-20 13:58:07 +0000

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router()
 
 
 router.route("/").get(getProducts).post(protect, admin, createProduct)
-router.route("/:id").get(getProductById)
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 export default router
