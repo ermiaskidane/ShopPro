a54659690f983020f0120f3df4e1a8feb021c24b
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2020-12-03 13:36:41 +0000

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,6 +8,13 @@ const getProducts = asyncHandler(async(req, res) => {
     const pageSize = 3
     const page = Number(req.query.pageNumber) || 1
     
+    const keyword = req.query.keyword ? {
+        name: {
+            $regex: req.query.keyword,
+            $options: "i"
+        }
+    } : {}
+    
     const products = await Product.find({})
     
     res.json(products)
